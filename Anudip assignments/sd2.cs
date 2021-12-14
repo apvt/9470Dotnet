a7318b1e437c4ffeca350adb9aba6d165ef28ac1
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2021-12-14 11:53:24 +0530

    Add files via upload

---
+++ Anudip assignments/sd2.cs
@@ -0,0 +1,16 @@
+using System;
+namespace sd2
+{
+ class sd2
+ {
+ public static void Main(string[] args)
+ {
+ string[] arr = new string[] { "mango", "apple", "guava" };
+ foreach(string a in arr)
+ {
+ Console.WriteLine(a);
+ }
+ Console.ReadLine();
+ }
+ }
+}
