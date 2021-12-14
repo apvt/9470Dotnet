cc0034303565a238b0b5c07e71d9ea9557b366ba
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2021-12-14 12:22:40 +0530

    Add files via upload

---
+++ Anudip assignments/md2.cs
@@ -0,0 +1,20 @@
+using System; 
+namespace md2 
+public class md2 
+{  
+    public static void Main(string[] args)  
+    {  
+        int[,] arr=new int[3,3];
+        arr[0,1]=10; 
+        arr[1,2]=20;  
+        arr[2,0]=30;   
+        for(int i=0;i<3;i++)
+        {  
+            for(int j=0;j<3;j++)
+            {  
+                Console.Write(arr[i,j]+" ");  
+            }  
+            Console.WriteLine(); 
+        }  
+        }
+        }
---
+++ Anudip assignments/sd1.cs
@@ -0,0 +1,16 @@
+using System;
+namespace sd1
+{
+ class sd1
+ {
+ public static void Main(string[] args)
+ {
+ int[] arr = new int[] {10,20,30,40};
+ for(int i=0;i<4;i++)
+ {
+ Console.WriteLine(arr[i]);
+ }
+ Console.ReadLine();
+ }
+ }
+ }
