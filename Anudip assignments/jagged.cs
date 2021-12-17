9dcc58decd73ba4cfbc0c899b5e27e8ce3d43d5a
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2021-12-17 11:20:36 +0530

    Add files via upload

---
+++ Anudip assignments/jagged.cs
@@ -0,0 +1,25 @@
+using System;
+namespace jagged
+{
+ class jagged
+ {
+ public static void Main(string[] args)
+ {
+  int[][] arr = new int[][]
+ {
+ new int[] {1,2},
+ new int[] {21,23,45},
+ new int[] {20,14,36,57,190 }
+ };
+ for(int i=0; i<arr.Length;i++)
+ {
+ for(int j=0;j<arr[i].Length; j++)
+ {
+ System.Console.Write(arr[i][j] + " ");
+ }
+ System.Console.WriteLine();
+ }
+ Console.ReadLine();
+ }
+ }
+} 
---
+++ Anudip assignments/var.cs
@@ -0,0 +1,16 @@
+using System; 
+ namespace Program
+ { 
+class ImplicitTypedExample 
+{ 
+ public static void Main() 
+{ 
+var a = 100; 
+var s = "Paris"; 
+var arr = new[] {10,20,30,40}; 
+Console.WriteLine(a); 
+Console.WriteLine(s); 
+Console.WriteLine(arr[1]); 
+ }
+ }
+ }
