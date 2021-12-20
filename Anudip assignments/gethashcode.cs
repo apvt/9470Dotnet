2c076eb91b8cb01195dc58a60f08e4a7be7e3826
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2021-12-20 11:59:19 +0530

    Add files via upload

---
+++ Anudip assignments/gethashcode.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+namespace object_Class_Gethashcode
+{
+ class Program
+ {
+ class HashC
+ {
+ string name;
+ public HashC(string name)
+ {
+ this.name = name;
+ }
+ public override int GetHashCode()
+ {
+ string n = ToString();
+ return n.GetHashCode(); 
+ }
+ 
+ }
+ static void Main(string[] args)
+ {
+ HashC ob1 = new HashC("Bari");
+ Console.WriteLine(ob1.GetHashCode());
+ Console.ReadLine();
+ }
+ }
+}
---
+++ Anudip assignments/tostring.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+namespace object_class_string
+{
+ class Program
+ {
+ static void Main(string[] args)
+ {
+ string name = "Bari Apoorva Bhagwan";
+ int age = 22;
+ float marks = 90.50f;
+ Console.WriteLine(name);
+ Console.WriteLine(age.ToString());
+ Console.WriteLine(marks.ToString());
+ Console.ReadLine();
+ }
+ }
+}
