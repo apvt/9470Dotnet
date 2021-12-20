9d1d32197de92de29a3f453826fc6f89a1b272b0
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2021-12-20 11:10:53 +0530

    Add files via upload

---
+++ Anudip assignments/as.cs
@@ -0,0 +1,20 @@
+using System;
+public class asoperator
+{
+public static void Main()
+ {
+object[] obj = new object[2];
+obj[0] = "Manali";
+obj[1] = 20;
+for (int i = 0; i < obj.Length; ++i)
+ {
+string s = obj[i] as string;
+Console.Write("{0}: ", i);
+if (s != null)
+Console.WriteLine("'" + s + "'");
+else
+Console.WriteLine("This is not a string!");
+}
+Console.ReadKey();
+}
+}
