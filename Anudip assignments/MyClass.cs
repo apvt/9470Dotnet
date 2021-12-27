74a67dbe3414d60a27196cc0605c3d525f9105b4
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2021-12-27 18:00:33 +0530

    Update MyClass.cs

--- Anudip assignments/MyClass.cs
+++ Anudip assignments/MyClass.cs
@@ -1,26 +1,24 @@
 using System;
-    
-    class Program{
-      public static void Main()
-        { }
- class MyClass
-        {
-             string myField = string.Empty;
-            public MyClass()
-            {
-            }
-             public void MyMethod(int parameter1, string parameter2)
-            {
-           Console.WriteLine("First Parameter {0}, second parameter {1}",parameter1, parameter2);
-            }
-             int MyAutoImplementedProperty { get; set; }
-            private int myPropertyVar;
-
-            public int MyProperty
-            {
-                get { return myPropertyVar; }
-                set { myPropertyVar = value; }
-            }
-        }
-    }
-    
+class MyClass
+{
+string myField = string.Empty;
+ MyClass(){}
+static void MyMethod(int parameter1, string parameter2)
+{
+Console.WriteLine("First Parameter {0}, Second parameter {1}",parameter1, parameter2);
+ }
+ private int myPropertyVar;
+ public int MyProperty
+ {
+  get { return myPropertyVar; }
+  set { myPropertyVar = value; }
+ }
+ public static void Main()
+ {
+   MyClass my = new MyClass();
+    my.myPropertyVar = 10;
+    Console.WriteLine(my.myPropertyVar);
+ }
+ }
+ 
+ 
