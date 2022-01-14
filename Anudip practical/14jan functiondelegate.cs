ce7bdcb272100330d6477283aa08f3a67f07a080
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-01-14 12:27:20 +0530

    Add files via upload

---
+++ Anudip practical/14jan functiondelegate.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Func
+{
+class Program
+{
+ static int Sum(int x, int y)
+ {
+ return x + y;
+ }
+ static void Main(string[] args)
+ {
+ Func<int,int, int> add = Sum;
+ int result = add(10, 10);
+ Console.WriteLine(result); 
+ }
+}
+}
