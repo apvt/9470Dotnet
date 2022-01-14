cfa00085e1c1c44db67c8d35ef594ae8e9ef9231
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-01-14 12:28:28 +0530

    Add files via upload

---
+++ Anudip practical/14jan anonymouscast.cs
@@ -0,0 +1,17 @@
+using System;  
+  
+public delegate void Test();  
+  
+public class Program  
+{  
+static int Main()  
+{  
+Test Display = delegate()  
+{  
+Console.WriteLine("Anonymous Delegate method");  
+};  
+  
+Display();  
+return 0;  
+}  
+}  
