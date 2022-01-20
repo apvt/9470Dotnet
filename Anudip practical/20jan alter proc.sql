9cf66098f0a454c6099517ebb5c9dfe0a1104cd6
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-01-20 15:12:39 +0530

    Add files via upload

---
+++ Anudip practical/20jan alter proc.sql
@@ -0,0 +1,29 @@
+--create procedure
+
+create procedure pro_emp 
+AS 
+BEGIN
+SELECT 
+empname,empcity,empphno
+FROM emp
+order by empname asc;
+end;
+
+--execute
+execute pro_emp;
+
+exec dbo.pro_emp;
+
+--alter 
+ALTER PROCEDURE pro_emp
+AS
+BEGIN
+SELECT
+empname,empcity,empsal
+FROM
+emp
+WHERE
+empsal=20000;
+END;
+
+
