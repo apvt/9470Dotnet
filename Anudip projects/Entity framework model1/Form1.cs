a285801c1b9c4ea45ce97c23624af1a3c49a69b5
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-02-04 11:17:54 +0530

    Add files via upload

---
+++ Anudip projects/Entity framework model1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           /* using (TECHNETEntities db = new TECHNETEntities())
+            {
+                Articoli art = db.Articoli.Where((x) => x.CodArt == "ART001").FirstOrDefault();
+
+                MessageBox.Show(art.DesArt);
+            }*/
+
+            using (TECHNETEntities db = new TECHNETEntities())
+            {
+                var art = db.Articoli
+                            .Join(db.Famiglie,
+                                  articolo => articolo.CodFamiglia,
+                                  famiglia => famiglia.CodFamiglia,
+                                  (articolo, famiglia) => new { Articoli = articolo, Famiglie = famiglia })
+                            .Where((x) => x.Articoli.CodArt == "ART005")
+                            .FirstOrDefault();
+
+                MessageBox.Show(art.Articoli.DesArt + " - " + art.Famiglie.DesFamiglia);
+            }
+        }
+    }
+}
+
