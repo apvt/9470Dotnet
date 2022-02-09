d2321deeb45c29188608866644ceec60fa18e748
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-02-09 15:54:25 +0530

    Add files via upload

---
+++ Anudip assignments/LINQ/LINQTOSTRINGS1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LINQTOSTRINGS
+{
+    class LINQTOSTRINGS1
+    {
+        static void Main(string[] args)
+        {
+            //create a string 'str1' 
+            string str1 = "Welcome to Java T Point.Com";
+            //here StringSplitOptions.RemoveEmptyEntries() is used to remove the spaces between the words. 
+            var result = from s in str1.ToLowerInvariant().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                         select s;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
+
