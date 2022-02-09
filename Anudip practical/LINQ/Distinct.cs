8b3e2bd5a1b3df4706b2ac9af0f13f04dc68abb4
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-02-09 12:15:09 +0530

    Add files via upload

---
+++ Anudip practical/LINQ/Distinct.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetOperators
+{
+    class Distinct
+    {
+        static void Main(string[] args)
+        {
+            string[] countries = { "UK", "India", "Australia", "uk", "india", "USA" }; IEnumerable<string> result = countries.Distinct(StringComparer.OrdinalIgnoreCase); foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ Anudip practical/LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, -6, 3, 0, -4, 25 };
+            // Query that selects only positive numbers.
+            var result = from n in nums
+                         where n > 0
+                         select n;
+            Console.Write("The positive values in nums: ");
+            // Execute the query.
+            foreach (int i in result)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }    
+}
