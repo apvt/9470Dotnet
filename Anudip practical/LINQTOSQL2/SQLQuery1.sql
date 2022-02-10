b832850300a4070f39f21a98286b3a0bbc32edb1
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-02-10 12:21:29 +0530

    Add files via upload

---
+++ Anudip practical/LINQTOSQL2/SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[EmployeeDetails](
+[EmpId] INT IDENTITY (1, 1) NOT NULL,
+[EmpName] VARCHAR (50) NULL,
+[Location] VARCHAR (50) NULL,
+[Gender] VARCHAR (20) NULL
+PRIMARY KEY CLUSTERED ([EmpId] ASC)
+);
+
+Insert into EmployeeDetails(EmpName,Location,Gender) values('minal','qwe','female');
+
