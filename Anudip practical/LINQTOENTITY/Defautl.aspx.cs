316257eba56d6733edeae30fc4293b808aed3251
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-02-11 12:22:12 +0530

    Add files via upload

---
+++ Anudip practical/LINQTOENTITY/Defautl.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQTOENTITY
+{
+    public partial class Defautl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindGridData();
+            }
+        }
+        private void BindGridData()
+        {
+            // Create the object of the Context defined in the constructor of the context class.
+            apoorvaEntities db = new apoorvaEntities();
+            var result = from emp in db.EmployeeDetails1
+                         select new
+                         {
+                             Id = emp.EmpId,
+                             Name = emp.EmpName,
+                             Location = emp.Location,
+                             Gender = emp.Gender
+                         };
+            GridView1.DataSource = result.ToList();
+            GridView1.DataBind();
+        }
+    }
+}
