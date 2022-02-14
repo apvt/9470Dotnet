421d13747c89a024bbaa86b5c6ae54a000416542
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-02-14 11:44:48 +0530

    Add files via upload

---
+++ Anudip practical/AllValidations/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AllValidation
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                lblmsg.Text = "Thank You";
+            }
+            else
+            {
+                lblmsg.Text = "Fill up all the fields";
+            }
+        }
+    }
+}
