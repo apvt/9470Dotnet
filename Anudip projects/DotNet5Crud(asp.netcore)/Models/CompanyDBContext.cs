7ab130d5f552b0aab72e55d8a5f8c4b7dc23d32c
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-03-09 10:59:29 +0530

    Add files via upload

---
+++ Anudip projects/DotNet5Crud(asp.netcore)/Models/CompanyDBContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+#nullable disable
+
+namespace DotNet5Crud.Models
+{
+    public partial class CompanyDBContext : DbContext
+    {
+        public CompanyDBContext(DbContextOptions<CompanyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=APOORVA;Database=CompanyDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Address)
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Designation)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.JoiningDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("decimal(18, 0)");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Anudip projects/DotNet5Crud(asp.netcore)/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace DotNet5Crud.Models
+{
+    public class EmployeeValidator
+    {
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Employee Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Employee Salary")]
+        public decimal Salary { get; set; }
+
+        [Required]
+        [Display(Name = "Joining Date")]
+        public decimal JoiningDate { get; set; }
+    }
+
+    [ModelMetadataType(typeof(EmployeeValidator))]
+    public partial class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Designation { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime JoiningDate { get; set; }
+    }
+}
