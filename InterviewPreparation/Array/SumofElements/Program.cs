f4be1866e1f508d1d89481c4799f60fe1145eeea
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-01 13:04:22 +0530

    Add files via upload

---
+++ InterviewPreparation/Array/SumofElements/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumofElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            int sum = array.Sum();
+            Console.WriteLine(sum);
+        }
+    }
+}
