2268e5c29dfe758a453a3dd847f5f275fe714a83
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-01 13:03:58 +0530

    Add files via upload

---
+++ InterviewPreparation/Array/StoreElementsInArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreElementsInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            foreach (int i in array)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+       
+    }
+}
