3413156b56f6817ae328dbb252344f7b4cfad010
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-01 13:08:24 +0530

    Add files via upload

---
+++ Lab Exercises/ArrayList/ArrayList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+class GFG
+{
+
+	// Main Method
+	public static void Main(string[] args)
+	{
+
+		// Create a list of strings
+		ArrayList al = new ArrayList();
+		al.Add("Apoorva");
+		al.Add("Shruti");
+		al.Add(10);
+		al.Add(10.10);
+
+		// Iterate list element using foreach loop
+		foreach (var names in al)
+		{
+			Console.WriteLine(names);
+		}
+	}
+}
