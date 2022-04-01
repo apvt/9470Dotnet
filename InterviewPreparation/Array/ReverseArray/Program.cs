64b1920d8cf38b7267c508f7cb30ef83934969cb
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-01 13:02:30 +0530

    Add files via upload

---
+++ InterviewPreparation/Array/ReverseArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 29, 15, 30, 98 };
+            int[] temp = list;
+            Console.Write("Original Array: ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            // reverse the array
+            Array.Reverse(temp);
+            Console.Write("Reversed Array: ");
+            foreach (int i in temp)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
