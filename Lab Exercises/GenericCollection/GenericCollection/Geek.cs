973a7a5747be065554463bae0b2af983b894a3df
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-01 12:49:38 +0530

    Add files via upload

---
+++ Lab Exercises/GenericCollection/GenericCollection/Geek.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Geek
+{
+
+	// Main Method
+	public static void Main(string[] args)
+	{
+
+		// Creating a List of integers
+		List<int> mylist = new List<int>();
+
+		// adding items in mylist
+		for (int j = 4; j < 10; j++)
+		{
+			mylist.Add(j * 3);
+		}
+
+		// Displaying items of mylist
+		// by using foreach loop
+		foreach (int items in mylist)
+		{
+			Console.WriteLine(items);
+		}
+	}
+}
