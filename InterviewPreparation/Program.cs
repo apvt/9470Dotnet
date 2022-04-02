a323ea9201c5d3429c34032f753d545433d8cc80
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-02 15:55:02 +0530

    Add files via upload

---
+++ InterviewPreparation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenOdd
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter number\t");
+            num=Convert.ToInt32(Console.ReadLine());
+            if (num % 2==0){
+                Console.WriteLine("{0}\tIs Even Number",num);
+            }
+            else
+            {
+                Console.WriteLine("{0}\tIs Odd Number",num);
+            }  
+            Console.ReadLine();
+        }
+    }
+}
