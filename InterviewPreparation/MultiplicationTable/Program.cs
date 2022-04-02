ecbc9ccb1d67df07233568de1c8240517c3994ec
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-02 16:19:19 +0530

    Add files via upload

---
+++ InterviewPreparation/MultiplicationTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j, i;
+
+            Console.Write("\n\n");
+            Console.Write("Multiplication table of:");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            for (j = 1; j <= 10; j++)
+            {
+                Console.Write("{0} X {1} = {2} \n", i, j, i * j);
+            }
+        }
+    }
+}
