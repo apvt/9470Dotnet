ca23459aaa41fda1d4798412f85af8e3ff8aa9d9
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-02 16:10:55 +0530

    Add files via upload

---
+++ InterviewPreparation/percentagevalue/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace percentagevalue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, percentage, option;
+            float result;
+
+        label:
+            Console.Write("\nEnter number:\t");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nEnter Percentage Value:\t");
+            percentage = Convert.ToInt32(Console.ReadLine());
+
+            result = (float)(num * percentage) / 100;
+            Console.WriteLine("Percentage value is:\t{0}", result);
+            Console.Write("\nCalculate again press 1");
+            option = Convert.ToInt32(Console.ReadLine());
+            if (option == 1)
+            {
+                goto label;
+            }
+            Console.ReadLine();
+        }
+    }
+}
