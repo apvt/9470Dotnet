bd751c03307cff19a66f9736df8eac4e104a0675
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-02 15:57:53 +0530

    Add files via upload

---
+++ InterviewPreparation/loops/LoopExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LoopExample
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0, j=0;
+            for(i=0;i<=5;i++)
+            {
+                for(j=0;j<i;j++)
+                {
+                    Console.Write(i);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
