498d32b2e906b61ca78578a98494d4ffa7e2b331
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-02 15:56:08 +0530

    Add files via upload

--- InterviewPreparation/Program.cs
+++ InterviewPreparation/Program.cs
@@ -1,21 +1,42 @@
 ﻿using System;
 
-namespace EvenOdd
+namespace FindADay
 {
      class Program
     {
         static void Main(string[] args)
         {
-            int num;
-            Console.Write("Enter number\t");
-            num=Convert.ToInt32(Console.ReadLine());
-            if (num % 2==0){
-                Console.WriteLine("{0}\tIs Even Number",num);
-            }
-            else
+            int a;
+        label:
+            Console.WriteLine("\nEnter a number(1-7)\t");
+            a=Convert.ToInt32(Console.ReadLine());
+            switch(a)
             {
-                Console.WriteLine("{0}\tIs Odd Number",num);
-            }  
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+               case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid option\n");
+                    goto label;
+            }
             Console.ReadLine();
         }
     }
