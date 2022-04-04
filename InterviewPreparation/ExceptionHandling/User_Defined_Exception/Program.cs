87ade3a9eab327a5410b73681ca5b6120f6c810f
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-04 17:25:42 +0530

    Add files via upload

---
+++ InterviewPreparation/ExceptionHandling/User_Defined_Exception/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace User_Defined_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Welcome");
+            a = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                if (a == 5 || a < 5)
+                {
+                    Console.WriteLine("Congratulations you have got an{0}", a);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    throw (new OutOfStockException("OutOfStockException Generated"));
+                }
+            }
+            catch (OutOfStockException oe)
+            {
+                Console.WriteLine(oe.Message.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+      //creating custom Exception
+      public class OutOfStockException : Exception
+      {
+          public OutOfStockException(string message) : base(message)
+          {
+                        
+          }
+      }
