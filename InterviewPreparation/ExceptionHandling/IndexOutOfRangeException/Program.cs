bd5886ede38f8a1a23671638e38b22356d5d56d7
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-04 17:19:52 +0530

    Add files via upload

---
+++ InterviewPreparation/ExceptionHandling/IndexOutOfRangeException/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Index_OutOfRange_Exception
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] a = new int[4];
+
+            try
+            {
+                Console.WriteLine("Enter the elements:");
+                for (int i = 0; i <= 4; i++)
+                {
+                    a[i] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
