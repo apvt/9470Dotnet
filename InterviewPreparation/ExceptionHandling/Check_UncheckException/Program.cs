1b4e3d2ecd388f5a5914355185b4ccaf64316792
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-04 17:18:44 +0530

    Add files via upload

---
+++ InterviewPreparation/ExceptionHandling/Check_UncheckException/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Check_UncheckException
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte num1 = 20, num2 = 31, result;
+            try
+            {
+                unchecked {
+                    result = (sbyte)(num1 * num2);
+                    Console.WriteLine("{0}x{1}={2}", num1, num2, result);
+                }
+                checked
+                {
+                    result = (sbyte)(num1 * num2);
+                    Console.WriteLine("{0}x{1}={2}", num1, num2, result);
+                }
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine(oe.Message);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
