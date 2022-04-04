182a07179390c41ba44e5876b2802d31438db16b
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-04 17:24:45 +0530

    Add files via upload

---
+++ InterviewPreparation/ExceptionHandling/NullReferenceException/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Null_Reference_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = null;
+            try
+            {
+                int length = value.Length;
+                Console.WriteLine(length);
+                Console.ReadLine();
+            }
+            catch (NullReferenceException ne)
+            {
+                Console.WriteLine(ne.Message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
