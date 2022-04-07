65efbf0c4be3847f1d3e3d17b85cd530b2bb912c
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-07 16:05:30 +0530

    Add files via upload

---
+++ Lab Exercises/SubString/Program.cs
@@ -0,0 +1,22 @@
+﻿//program to display all Substrings present in the given String
+
+using System;
+
+namespace SubString
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "abc";
+            for(int i = 1; i<str.Length; i++)
+            {
+                for(int j = 0; j<str.Length; j++)
+                {
+                    string substr = str.Substring(j, i);
+                    Console.WriteLine(substr);
+                }
+            }
+        }
+    }
+}
