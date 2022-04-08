079b74618bcb5183b8ad8a5719da46f34b5ea2af
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-08 11:52:36 +0530

    Add files via upload

---
+++ InterviewPreparation/Generic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Generic
+{
+    //declare generics
+    public class GenericClass<T>
+    { public void Genericfunction(T printvalue)
+    {
+        Console.WriteLine(printvalue);
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("printing integer values");
+        GenericClass<int> i = new GenericClass<int>();
+        i.Genericfunction(500);
+        Console.WriteLine("printing string values");
+        GenericClass<string> s = new GenericClass<string>();
+        s.Genericfunction("Apoorva");
+    }
+}
+    }
