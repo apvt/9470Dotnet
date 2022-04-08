d4bbf410303c367d45345dc307698bdb9a387aa8
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-08 11:50:20 +0530

    Add files via upload

---
+++ InterviewPreparation/pattern1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace pattern1
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i, j, k;
+            Console.Write("Input number of rows : ");
+            k = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i <= k; i++)
+            {
+                /* print blank spaces */
+                for (j = 1; j <= k - i; j++)
+                    Console.Write(" ");
+                /* Display number in asc order till middle*/
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0}", j);
+
+                /* Display  number in reverse order after middle */
+                for (j = i - 1; j >= 1; j--)
+                    Console.Write("{0}", j);
+
+                Console.Write("\n");
+            }
+        }
+
+    }
+}
