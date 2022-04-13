cc8a67fcd756c350762db30d24531c250d9c31cc
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-13 10:48:57 +0530

    Add files via upload

---
+++ InterviewPreparation/addfunction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace addfunction
+{
+    class sum
+    {
+        static int num1, num2, result;
+        public static void add()
+        {
+            Console.Write("Enter number 1st.\t");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter number 2nd.\t");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            result = num1 + num2;
+
+            Console.Write("\nAdd = {0}", result);
+            Console.ReadLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sum.add();
+        }
+    }
+}
