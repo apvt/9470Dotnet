9bb566e305f337689816a435ee322bc5f8ffa9ab
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-14 10:53:45 +0530

    Add files via upload

---
+++ Lab Exercises/NegativeElements/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NegativeElements
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[100];
+            int i,n;
+            Console.Write("Input  elements in the array :\n");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("{0} : ", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < n; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    Console.WriteLine("Negative elements in array are : {0}",arr[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
