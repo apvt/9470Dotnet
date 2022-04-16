d493a55bf4ba176a9f57f49cfc194fa4df454d91
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-16 14:47:35 +0530

    Add files via upload

---
+++ InterviewPreparation/ArrayStoresNames/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayStoresNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 3;
+            Person[] persons = new Person[total];
+            Console.WriteLine("Enter three names:");
+            for (int i = 0; i < total; i++)
+            {
+                persons[i] = new Person()
+                {
+                    Name = Console.ReadLine()
+                };
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return "Hello! My name is " + Name;
+            }
+        }
+    }
+}
