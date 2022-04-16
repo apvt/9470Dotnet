4b4dea94954ff2adfb4d64fd9edef1f317b1811c
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-16 15:22:33 +0530

    Add files via upload

---
+++ InterviewPreparation/SeparateString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SeparateString
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string str;
+            int a = 0;
+
+            Console.Write("Input the string : ");
+            str = Console.ReadLine();
+            Console.Write("The characters of the string are  :  ");
+            while (a <= str.Length - 1)
+            {
+                Console.Write("{0} ", str[a]);
+                a++;
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
+
+    
