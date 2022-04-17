8c76e2b836870b232a7629b6273957349bf16a7e
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-17 15:37:25 +0530

    Add files via upload

---
+++ InterviewPreparation/SumofNo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumofNo
+{
+     class Program
+    {
+        public static void Main()
+        {
+            int j, sum = 0;
+
+            Console.Write("The first 10 natural number are :\n");
+            for (j = 1; j <= 10; j++)
+            {
+                sum = sum + j;
+                Console.Write("{0} ", j);
+            }
+            Console.Write("\nThe Sum is : {0}\n", sum);
+        }
+    }
+}
