6ea6efcc3702ff5174d0a02d2503407839cd7122
ApoorvaBari <94681518+apvt@users.noreply.github.com>
2022-04-17 15:39:36 +0530

    Add files via upload

---
+++ InterviewPreparation/StarPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StarPattern
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, rows;
+            Console.Write("Input number of rows : ");
+            rows = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= rows; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+        }
+    }
+}
